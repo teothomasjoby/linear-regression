db855b8f8bc18452ffe95cacad5bba32eb510710
teo.joby <teo.joby@qburst.com>
2024-10-14 12:49:27 +0530

    code for weight updating

--- multi_feature.py
+++ multi_feature.py
@@ -1,38 +1,37 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self):
-        self.W = [0.0] * 7
 
     def predict(self, X):
         predictions = []
         for row in X:
-            prediction = self.W[0] 
+            prediction = self.W[0]
             for i in range(len(row)):
                 prediction += self.W[i + 1] * row[i]
             predictions.append(prediction)
         return predictions
-    
-    def fit(self, X, y, epochs=1000, learning_rate=0.01):
-        m = len(y)
-        self.W = np.zeros(7)
+
+    def fit(self, X, y, epochs=1000, learning_rate=0.001):
+        m, n = X.shape
+        self.W = np.zeros(n+1)
 
         for epoch in range(epochs):
-            mse = 1/m * np.sum((self.predict(X)-y)**2)
+            error = self.predict(X)-y
+            mse = 1/m * np.sum(error**2)
             print(mse)
-            weights = []
-            for i in range(m):
-                y_pred = self.predict([X[i]])[0]
-                weights[0] = 1/m * np.sum((self.predict(X)-y)**2)
-                for j in range()
-                error = y_pred - y[i]
-            #     gradients[0] += error  # Gradient for bias term
-            #     for j in range(6):
-            #         gradients[j + 1] += error * X[i][j]  # Gradient for feature j
-            
-            # # Update weights
-            # for k in range(7):
-            #     self.weights[k] -= (learning_rate / m) * gradients[k]
+            dw = np.zeros(n+1)
+            dw[0] = 2/m * np.sum(error)  # partial derivative for bias
+
+            # calculate partial derivative for other weights
+            for j in range(n):
+                dw[j + 1] = 2 / m * np.sum(error * X[:, j])
+                # X[:, j] is the vector of all X values in the data
+
+            # updating weights
+            self.W[0] -= (learning_rate) * dw[0]
+            for k in range(1, n+1):
+                self.W[k] -= (learning_rate) * dw[k]
+
 
 X = np.array([
     [1, 2, 3, 4, 5, 6],
@@ -40,10 +39,16 @@ X = np.array([
     [3, 4, 5, 6, 7, 8],
     [4, 5, 6, 7, 8, 9]])
 
+# X = np.array([
+#     [1, 2, 3, 4],
+#     [2, 3, 4, 5],
+#     [3, 4, 5, 6],
+#     [4, 5, 6, 7]])
+
 y = np.array([10, 12, 14, 16])
 
 model = LinearRegression()
-model.fit(X, y, epochs=1, learning_rate=0.001)
+model.fit(X, y, epochs=1000, learning_rate=0.001)
 
 predictions = model.predict(X)
 print("Predictions:", predictions)

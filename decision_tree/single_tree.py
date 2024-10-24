ca160fc490082e575e748172aa8ef3524a509f5b
teo.joby <teo.joby@qburst.com>
2024-10-24 17:54:07 +0530

    code for best split

--- decision_tree/single_tree.py
+++ decision_tree/single_tree.py
@@ -1,3 +1,5 @@
+# ref: https://github.com/Suji04/ML_from_Scratch/blob/master/decision%20tree%20regression.ipynb
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -14,7 +16,37 @@ class DecisionTreeClassifier:
         best_split = {}
 
         if num_samples >= self.min_samples_split and curr_depth <= self.max_depth:
-            best_split = self.get_best_split()
+            best_split = self.get_best_split(X, y, num_samples, num_features)
+
+    def get_best_split(self, X, y, num_samples, num_features):
+        dataset = np.concatenate((X, y), axis=1)
+        best_split = {}
+
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_threshold = np.unique(feature_values)
+            for threshold in possible_threshold:
+                dataset_left, dataset_right = self.split(
+                    dataset, feature_index, threshold
+                )
+                if len(dataset_left) > 0 and len(dataset_right) > 0:
+                    target, left_y, right_y = (
+                        dataset[:, -1],
+                        dataset_left[:, -1],
+                        dataset_right[:, -1],
+                    )
+                    curr_var_red = self.variance_reduction(target, left_y, right_y)
+            print("feature_values", feature_values)
+            print("possible_threshold", possible_threshold)
+
+    def split(self, dataset, feature_index, threshold):
+        database_left = np.array(
+            [row for row in dataset if row[feature_index] <= threshold]
+        )
+        database_right = np.array(
+            [row for row in dataset if row[feature_index] > threshold]
+        )
+        return database_left, database_right
 
 
 col_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "type"]
@@ -30,4 +62,7 @@ y = data.iloc[:, -1].values.reshape(-1, 1)
 train_X, test_X, train_y, test_y = train_test_split(
     X, y, test_size=0.2, random_state=41
 )
-print(len(train_X), train_y)
+model = DecisionTreeClassifier(2, 2)
+model.build_tree(train_X, train_y)
+
+print(np.concatenate((train_X, train_y), axis=1))
